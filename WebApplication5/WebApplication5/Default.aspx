24d8b0c489d583c6798f786a29593717f8b3421e
Eric Romero <youngmero2@gmail.com>
2016-04-08 21:46:40 -0500

    first commit

---
+++ WebApplication5/WebApplication5/Default.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MFNCheckOut._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>CHECKOUT PAGE</h1>
+    </div>
+
+    <div class="row">
+       
+        <!-- 
+            city = "Johnstown",
+            country_code = "US",
+            line1 = "52 N Main ST",
+            postal_code = "43210",
+            state = "OH"
+        },
+            cvv2 = "874",
+            expire_month = 11,
+            expire_year = 2018,
+            first_name = "Joe",
+            last_name = "Shopper",
+            number = "4877274905927862",
+            type = "visa"
+        -->
+
+         <div class="col-md-3">
+            <h2>Payment Information</h2>
+             <p>Please Enter First Name: </p>
+             <asp:TextBox ID="first_name" runat="server" Text="Joe"></asp:TextBox>
+             <p>Please Enter Last Name: </p>
+             <asp:TextBox ID="last_name" runat="server" Text="Shopper"></asp:TextBox>
+             <p>Please Enter Country: </p>
+             <asp:DropDownList ID="country_code" runat="server">
+                 <asp:ListItem Value="US">United States</asp:ListItem>
+                 <asp:ListItem Value="CA">Canada</asp:ListItem>
+             </asp:DropDownList>
+             <p>Please Enter Street Address: </p>
+             <asp:TextBox ID="address" runat="server" Text="52 N Main ST"></asp:TextBox>
+             <p>Please Enter City: </p>
+             <asp:TextBox ID="city" runat="server" Text="Johnstown"></asp:TextBox>
+             <p>Please Enter Zip Code: </p>
+             <asp:TextBox ID="postal_code" runat="server" Text="43210"></asp:TextBox>
+             <asp:RegularExpressionValidator ID="RegularExpressionValidator5" ControlToValidate="postal_code" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>
+             <p>Please Enter State: </p>
+             <asp:TextBox ID="state" runat="server" Text="OH"></asp:TextBox>
+        </div>
+
+        <div class="col-md-4">
+            <p>Please Enter Card Type: </p>
+            <asp:DropDownList ID="DropDownList2" runat="server">
+                 <asp:ListItem Value="visa">visa</asp:ListItem>
+                 <asp:ListItem Value="mastercard">Master Card</asp:ListItem>
+             </asp:DropDownList>
+
+            <p>Creadit Card Number: </p>
+             <asp:TextBox ID="card_number" runat="server" Text="4877274905927862"></asp:TextBox>
+             <asp:RegularExpressionValidator ID="RegularExpressionValidator1" ControlToValidate="card_number" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>
+             
+            <p>cvv: </p>
+             <asp:TextBox ID="cvv2" runat="server" Text="874"></asp:TextBox>
+             <asp:RegularExpressionValidator ID="RegularExpressionValidator2" ControlToValidate="cvv2" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>
+             
+            <p>Expiration Month: </p>
+             <asp:TextBox ID="ex_month" runat="server" Text="11"></asp:TextBox>
+             <asp:RegularExpressionValidator ID="RegularExpressionValidator3" ControlToValidate="ex_month" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>
+             
+            <p>Expiration Year: </p>
+             <asp:TextBox ID="ex_year" runat="server" Text="2018"></asp:TextBox>
+             <asp:RegularExpressionValidator ID="RegularExpressionValidator4" ControlToValidate="ex_year" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>
+             
+        </div>
+
+        <div class="col-md-5">
+            <asp:Button ID="Button1" runat="server" Text="Confirm Payment" OnClick="Button1_Click" />
+            
+        </div>
+        
+    
+    </div>
+
+</asp:Content>
---
+++ WebApplication5/WebApplication5/Default.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PayPal.Api;
+using PayPal.Sample;
+
+namespace MFNCheckOut
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // ### Api Context
+            // Pass in a `APIContext` object to authenticate 
+            // the call and to send a unique request id 
+            // (that ensures idempotency). The SDK generates
+            // a request id if you do not pass one explicitly. 
+            // See [Configuration.cs](/Source/Configuration.html) to know more about APIContext.
+            var apiContext = Configuration.GetAPIContext();
+
+            // A transaction defines the contract of a payment - what is the payment for and who is fulfilling it. 
+            var transaction = new Transaction()
+            {
+                amount = new Amount()
+                {
+                    currency = "USD",
+                    total = "1.50",
+                    details = new Details()
+                    {
+                        shipping = "0.50",
+                        subtotal = "0.50",
+                        tax = "0.50"
+                    }
+                },
+                description = "This is the payment transaction description.",
+                item_list = new ItemList()
+                {
+                    items = new List<Item>()
+                    {
+                        new Item()
+                        {
+                            name = "Session",
+                            currency = "USD",
+                            price = "0.50",
+                            quantity = "1",
+                            sku = "sku"
+                        }
+                    },
+                    shipping_address = new ShippingAddress
+                    {
+                        city = city.Text,
+                        country_code = country_code.Text,
+                        line1 = address.Text,
+                        postal_code = postal_code.Text,
+                        state = state.Text,
+                        recipient_name = first_name.Text + " " + last_name.Text
+                    }
+                },
+                invoice_number = Common.GetRandomInvoiceNumber()
+            };
+
+            // A resource representing a Payer that funds a payment.
+            var payer = new Payer()
+            {
+                payment_method = "credit_card",
+                funding_instruments = new List<FundingInstrument>()
+                {
+                    new FundingInstrument()
+                    {
+                        credit_card = new CreditCard()
+                        {
+                            billing_address = new Address()
+                            {
+                                city = city.Text,
+                                country_code = country_code.Text,
+                                line1 = address.Text,
+                                postal_code = postal_code.Text,
+                                state = state.Text
+                            },
+                            cvv2 = cvv2.Text,
+                            expire_month = Int32.Parse(ex_month.Text),
+                            expire_year = Int32.Parse(ex_year.Text),
+                            first_name = first_name.Text,
+                            last_name = last_name.Text,
+                            number = card_number.Text,
+                            type = DropDownList2.Text
+                        }
+                    }
+                },
+                payer_info = new PayerInfo
+                {
+                    email = "test@email.com"
+                }
+            };
+
+            // A Payment resource; create one using the above types and intent as `sale` or `authorize`
+            var payment = new Payment()
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = new List<Transaction>() { transaction }
+            };
+
+            // ^ Ignore workflow code segment
+            #region Track Workflow
+           // this.flow.AddNewRequest("Create credit card payment", payment);
+            #endregion
+
+            // Create a payment using a valid APIContext
+            var createdPayment = payment.Create(apiContext);
+
+            // ^ Ignore workflow code segment
+            #region Track Workflow
+          //  this.flow.RecordResponse(createdPayment);
+            #endregion
+
+            // For more information, please visit [PayPal Developer REST API Reference](https://developer.paypal.com/docs/api/).
+        }
+    }
+}
